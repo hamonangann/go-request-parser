3d750d5bb844c22ebd8ffaa97df3caae45eb3e45
hamonangann <bornyto.hamonangan@ui.ac.id>
2023-02-13 21:36:43 +0700

    add validator

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module go-request-parser
 go 1.19
 
 require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- main.go
+++ main.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
+// User role additional info: 1 for Admin, 2 for registered user, empty/0 for guest/visitor temp account
 type User struct {
-	Name  string `json:"name" form:"name" query:"name"`
-	Email string `json:"email" form:"email" query:"email"`
+	Name  string `json:"name" form:"name" query:"name" validate:"required"`
+	Email string `json:"email" form:"email" query:"email" validate:"required,email"`
+	Role  int    `json:"role" form:"role" query:"role" validate:"gte=0,lte=2"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	return cv.validator.Struct(i)
 }
 
 func main() {
 	r := echo.New()
+	r.Validator = &CustomValidator{validator: validator.New()}
 
 	r.Any("/user", func(c echo.Context) error {
 		u := new(User)
 		if err := c.Bind(u); err != nil {
-			return err
+			return c.JSON(http.StatusInternalServerError, "cannot bind data")
+		}
+		if err := c.Validate(u); err != nil {
+			return c.JSON(http.StatusBadRequest, "invalid request")
 		}
 		return c.JSON(http.StatusOK, u)
 	})
